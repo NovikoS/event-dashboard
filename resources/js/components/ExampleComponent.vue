c35661518e978d6b766d2934e6e0157351ddabf5
vladislav <v.novikov@napopravku.ru>
2022-05-16 19:58:32 +0300

    init

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <v-app-bar
+      color="red"
+      rounded
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+
+export default {
+  mounted() {
+    console.log('Component mounted.')
+  }
+}
+</script>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/@mdi/font@6.5.95/css/materialdesignicons.min.css">
+</head>
+<body>
+<div id="app">
+    <v-app>
+        <example-component></example-component>
+    </v-app>
+</div>
+<script src="js/app.js"></script>
+</body>
+</html>
