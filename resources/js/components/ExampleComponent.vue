09eb040d1be724930fb7cc655d08cb73c3b1b06c
vladislav <v.novikov@napopravku.ru>
2022-05-18 20:33:01 +0300

    Add table and list

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -2,7 +2,6 @@
   <div>
     <v-app-bar
       color="red"
-      rounded
     >
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
@@ -11,17 +10,51 @@
       <v-spacer></v-spacer>
 
       <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
-      <v-btn icon>
         <v-icon>mdi-dots-vertical</v-icon>
       </v-btn>
     </v-app-bar>
+
+    <!--LIST-->
+    <div>
+      <v-expansion-panels accordion>
+        <v-expansion-panel
+          v-for="(job,i) in jobs"
+          :key="i"
+        >
+          <v-expansion-panel-header>{{ job.name }}</v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <!--TABLE-->
+            <v-data-table
+              dense
+              :items="job.flow"
+              item-key="status"
+              :headers="headers"
+              class="elevation-1"
+            >
+              <template v-slot:item.data="{ item }">
+                <v-textarea
+                  :value="JSON.stringify(item.data)"
+                >
+                </v-textarea>
+              </template>
+
+              <template v-slot:item.errors="{ item }">
+                <v-textarea
+                  :value="JSON.stringify(item.errors)"
+                >
+                  {{ item.errors }}
+                </v-textarea>
+              </template>
+
+
+            </v-data-table>
+            <!--            -->
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </div>
+    <!--    -->
+
   </div>
 </template>
 
@@ -30,6 +63,90 @@
 export default {
   mounted() {
     console.log('Component mounted.')
-  }
+  },
+  data: () => ({
+    items: [
+      {
+        icon: 'mdi-inbox',
+        text: 'Inbox',
+      },
+      {
+        icon: 'mdi-star',
+        text: 'Star',
+      },
+      {
+        icon: 'mdi-send',
+        text: 'Send',
+      },
+      {
+        icon: 'mdi-email-open',
+        text: 'Drafts',
+      },
+    ],
+    model: 1,
+    jobs: [
+      {
+        name: 'qwtqwtweryrtu5e-by54wyb7y5-67567n56 7',
+        flow: [
+          {
+            status: 'processing',
+            errors: null,
+            data: {
+              contact:
+                {
+                  channels:
+                    [{
+                      type: "email",
+                      value: "v.novikov@napopravku.ru"
+                    }],
+                  address: {
+                    town: "spb"
+                  },
+                  id: 1287970279,
+                  contactKey: 442846
+                },
+              groups: [175942367],
+              formType: "homepageEmailSubscriptionForm"
+            }
+          },
+
+          {
+            status: 'failed',
+            errors: {
+              message: 'УПАЛО ПОТОМУ ЧТО'
+            },
+            data: '',
+          }
+        ],
+      },
+      {
+        name: 'qwtqwtweryrtu5e-by54wyb7y5-67567n56 7',
+        flow: [
+          {
+            status: 'processing',
+            errors: null,
+            data: {
+              userId: 442846
+            },
+          },
+
+          {
+            status: 'failed',
+            errors: {
+              message: 'УПАЛО ПОТОМУ ЧТО'
+            },
+            data: {
+              userId: 442846
+            },
+          }
+        ],
+      },
+    ],
+    headers: [
+      {text: 'Статус', value: 'status'},
+      {text: 'Ошибки', value: 'errors'},
+      {text: 'Данные', value: 'data'},
+    ],
+  }),
 }
 </script>
